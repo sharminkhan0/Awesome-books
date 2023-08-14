6110f17764a56d90ad087534e90e2dc040818548
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-08-14 15:17:52 +0600

    Modify index.html file

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-let books = [];
+const books = JSON.parse(localStorage.getItem('books')) || [];
 
+function updateBookList() {
+  const bookList = document.getElementById('booksList');
+
+  bookList.innerHTML = '';
+  books.forEach((book, index) => {
+    const li = document.createElement('li');
+    li.classList.add('book-details');
+    li.innerHTML = `
+    <div class="book-content">
+      <p>${book.title}</p> 
+      <p>by ${book.author}</p>
+    </div>`;
+
+    function removeBook(index) {
+      books.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(books));
+      updateBookList();
+    }
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+
+    removeBtn.addEventListener('click', () => removeBook(index));
+
+    li.appendChild(removeBtn);
+    bookList.appendChild(li);
+  });
+}
 
 function addBook(event) {
   event.preventDefault();
 
-  const titleInput = document.getElementById("title");
-  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById('title');
+  const authorInput = document.getElementById('author');
   const title = titleInput.value;
   const author = authorInput.value;
 
@@ -13,42 +41,13 @@ function addBook(event) {
 
   books.push(book);
 
-  titleInput.value = "";
-  authorInput.value = "";
-
-
-  updateBookList();
-}
-
-
-function removeBook(index) {
-
-  books.splice(index, 1);
-
+  localStorage.setItem('books', JSON.stringify(books));
+  titleInput.value = '';
+  authorInput.value = '';
 
   updateBookList();
 }
 
-
-function updateBookList() {
-  const bookList = document.getElementById("booksList");
-
-
-  bookList.innerHTML = "";
-
-
-  books.forEach((book, index) => {
-    const li = document.createElement("li");
-    li.textContent = `${book.title} by ${book.author}`;
-
-    const removeBtn = document.createElement("button");
-    removeBtn.textContent = "   Remove";
-    removeBtn.addEventListener("click", () => removeBook(index));
-
-    li.appendChild(removeBtn);
-    bookList.appendChild(li);
-  });
-}
-
-const bookForm = document.getElementById("bookForm");
-bookForm.addEventListener("submit", addBook);
+const bookForm = document.getElementById('bookForm');
+bookForm.addEventListener('submit', addBook);
+updateBookList();
